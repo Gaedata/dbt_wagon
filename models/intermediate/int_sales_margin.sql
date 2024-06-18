d5d3c302f641c1b0e31d7989008bacb989bf09ef
Gaëlle Robert <gaelle.robert3@gmail.com>
2024-06-18 14:43:02 +0000

    commit exo6

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -19,7 +19,7 @@ SELECT
 date_date,
 orders_id,
 products_id,
-CONCAT(orders_id,"_",products_id) as primary_key,
+CONCAT(orders_id,"_",products_id) as primary_key_sales,
 revenue,
 quantity,
 purchase_price,
