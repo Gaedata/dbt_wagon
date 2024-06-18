9f6b11b602d327b9567e65ee42b63ce445087b95
Gaëlle Robert <gaelle.robert3@gmail.com>
2024-06-18 13:17:51 +0000

    commit exo4 part fin 1

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,26 @@
-SELECT * FROM 
-{{ref("stg_raw__sales")}} 
+WITH test as (SELECT 
+date_date,
+orders_id,
+products_id,
+revenue,
+quantity,
+purchase_price,
+quantity*purchase_price as purchase_cost
+
+
+FROM 
+{{ref("stg_raw__sales")}}
 JOIN
 {{ref("stg_raw__product")}}
 USING (products_id)
+)
+
+SELECT
+date_date,
+orders_id,
+products_id,
+revenue,
+quantity,
+purchase_price,
+revenue-purchase_cost as margin
+FROM test
