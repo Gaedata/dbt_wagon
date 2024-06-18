b4668c2dc3877adff5654f387fff8940e33308da
Gaëlle Robert <gaelle.robert3@gmail.com>
2024-06-18 13:59:35 +0000

    commit fin

--- models/intermediate/int_orders_operational2.sql
+++ models/intermediate/int_orders_operational2.sql
@@ -1,12 +1,14 @@
 SELECT
 orders_id,
 date_date,
-(margin+shipping_fee)-(logcost-ship_cost) as Operational margin,
-revenue,
-qty,
-purchase_cost,
-margin
+SUM(revenue),
+SUM(qty),
+SUM(purchase_cost),
+SUM(margin),
+SUM(margin+shipping_fee-logcost-ship_cost) as operational
 FROM {{ref("int_orders_margin2")}}
 JOIN
 {{ref("stg_raw__ship")}}
 USING(orders_id)
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
