a944c26e9b8a5b95ddcd00935b0df6f845ed4e2b
Gaëlle Robert <gaelle.robert3@gmail.com>
2024-06-19 14:51:36 +0000

    commit fin

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,18 +1,8 @@
 WITH test_primary as (
-    
-SELECT *
-FROM {{ref("stg_raw__adwords")}}
-UNION ALL 
-SELECT *
-FROM {{ref("stg_raw__bing")}}
-UNION ALL 
-SELECT *
-FROM {{ref("stg_raw__criteo")}}
-UNION ALL 
-SELECT *
-FROM {{ref("stg_raw__facebook")}}
+{{dbt_utils.union_relations(relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]) }}
 )
 SELECT
 CONCAT(date_date,"_",campaign_key) as primary_key_campaign,
 *,
 FROM test_primary
+
